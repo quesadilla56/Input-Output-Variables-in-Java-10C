42195e50620908db5feeae4686c6e00f79b82b73
quesadilla56 <92122714+quesadilla56@users.noreply.github.com>
2021-10-08 12:11:34 -0700

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("dang!");
+  }
+}
+/*this is a comment*/
+//this is another way to comment
+/* Java Variable types
+You have five types of variables in Java
+Boolean: True/False, only 2 state - boolean
+Integers: Int, always whole numbers 
+Strings: Text, example "Hello" always between quotations
+Float: always decimals, example 19.98 or -19.98
+Char: single characters, "a" or "B"
+In math you say x=4 it means x=4 
+In programming x=4 (x is assigned to 4)
+
+x="patty"
+c="cheese"
+b="tomato"
+d="bread"
+e="lettuce"
+f="ketchup"
+g="mayo"
+
+So here is the template: type variable = value;
+*/ 
